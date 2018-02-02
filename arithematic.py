3d2e37664d3eb1b7ca8b6665e457b94ea0401808
jasmeet <jasmeet_1313012_2013@yahoo.com>
2018-02-02 23:08:10 +0530

    Created profile of the spy

---
+++ arithematic.py
@@ -0,0 +1,15 @@
+num1 = input("Enter the first number:")
+num2 = input("Enter the second number:")
+#Add Two Numbers
+sum = float(num1) + float(num2)
+print sum
+#Multiply Two Numbers
+mul = float(num1) * float(num2)
+print mul
+# Subtract two numbers
+min = float(num1) - float(num2)
+print min
+#Divide two numbers
+div = float(num1) / float(num2)
+print div
+
---
+++ main.py
@@ -0,0 +1,8 @@
+print "Hello World!!"
+print "let's get started"
+spy_name = raw_input("What is your name?")
+print "Hello" + spy_name + "\n Welcome in python"
+spy_salutation = raw_input("How may I call you ? Mr. or Ms. ")
+print spy_salutation + spy_name
+
+
